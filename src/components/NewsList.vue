62d1d0930ab8c5fc9ae76669bdc1af00de70d36d
TakashiZenkou <kai879@ymail.com>
2022-04-02 18:57:42 -0500

    Lab 6

---
+++ src/components/NewsList.vue
@@ -0,0 +1,63 @@
+<template>
+<div class="super">
+    <div v-for="article in articles" class="article">
+        <img :src="article.urlToImage"/>
+        <p class="title">{{article.title}}</p>
+        {{article.description}}
+        <div class="bottom-color"></div>
+    </div>
+</div>
+
+<form @submit.prevent="searchNews" class="d-flex flexcolumn justify-content-center">
+ <div class="input-group mx-sm-3 mb-2">
+ <label class="visually-hidden" for="search">Search</label>
+ <input type="search" name="search" v-model="searchTerm" id="search" class="form-control mb-2 mr-sm-2" placeholder="Enter search term here" />
+ <button class="btn btn-primary mb-2">Search</button>
+ </div>
+ <p>You are searching for {{ searchTerm }}</p>
+ </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+         articles: [],
+         searchTerm: ''
+    }
+    },
+    methods: {
+        searchNews() {
+            let self = this;
+            fetch('https://newsapi.org/v2/everything?q='+self.searchTerm + '&language=en', {
+        headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`,
+        }
+        })
+        .then(function(response) {
+        return response.json();
+        })
+        .then(function(data) {
+        console.log(data);
+        self.articles = data.articles;
+        });
+        }
+        },
+    created() {
+        let self = this;
+        fetch('https://newsapi.org/v2/top-headlines?country=us',
+    {
+    headers: {
+    'Authorization': `Bearer ${import.meta.env.VITE_NEWSAPI_TOKEN}`
+    }
+    })
+        .then(function(response) {
+        return response.json();
+        })
+        .then(function(data) {
+        console.log(data);
+        self.articles = data.articles;
+    });
+    }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,6 @@
 <script>
+
+
 export default {
     data() {
         return {
@@ -18,5 +20,58 @@ export default {
 </template>
 
 <style>
-/* Add any component specific styles here */
+
+.news{
+  text-align: center;
+}
+
+.super{
+  display:flex;
+  flex-direction:row;
+  flex-wrap:wrap;
+  justify-content: center;
+  }
+
+.article{
+  position: relative;
+   height:500px;
+   width: 250px;
+   margin-right:20px;
+   margin-top:20px;
+   border:2px solid rgb(232, 234, 237);
+   border-radius: 12px;
+   }
+
+img{
+    width:100%;
+    height: 150px;
+    }
+
+.title{
+  font-weight:600;
+  margin-top: 15px;
+  }
+
+.bottom-color{
+    height: 10px;
+    width: 100%;
+    background-color:rgb(92,192,146);
+    color:rgb(92,192,146);
+    border-radius:900px;
+    position: absolute;
+    bottom: 0px;  
+}
+
+form{
+  margin-top:30px;
+  display:flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+#search{
+  width:50% !important;
+  margin-left: 300px;
+  flex-grow: 0;
+}
 </style>
